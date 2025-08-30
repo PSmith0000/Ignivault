ad3807301d624f93681734c2a4b74b14de7cb323
PSmith0000 <40529988+PSmith0000@users.noreply.github.com>
2025-08-30 15:41:37 -0400

    Content Update
    Changes to Login Page
    Added Registration Page
    Web API Project Creation

---
+++ vs/Game/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vintagestory.Client.NoObf;
+namespace vs.Game
+{
+    internal class Game
+    {
+        public static NetworkAPI netAPI { get { return  NetworkAPI.Instance; } }
+    }
+}
---
+++ vs/Init.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs
+{
+    public class Init
+    {
+        public static void Load()
+        {
+            
+        }
+    }
+}
