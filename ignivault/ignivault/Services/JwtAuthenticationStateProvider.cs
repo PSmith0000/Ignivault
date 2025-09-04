8fa2e3aefe4e6ff4d8b2086e1b0f6f52ab49637c
PSmith0000 <40529988+PSmith0000@users.noreply.github.com>
2025-09-03 21:11:31 -0400

    Update Push
    Added: Auth to pages.
    Added: Profile Page
    Changed Various UI elements.

---
+++ ignivault/ignivault/Services/JwtAuthenticationStateProvider.cs
@@ -0,0 +1,65 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using System.IdentityModel.Tokens.Jwt;
+namespace ignivault.Services
+{
+    public class JwtAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly JwtSecurityTokenHandler _tokenHandler = new();
+
+        public JwtAuthenticationStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>("authToken");
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            var identity = GetClaimsPrincipalFromToken(token);
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+        }
+
+        public void NotifyUserAuthentication(string token)
+        {
+            var identity = GetClaimsPrincipalFromToken(token);
+            var user = new ClaimsPrincipal(identity);
+
+            var authState = Task.FromResult(new AuthenticationState(user));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void NotifyUserLogout()
+        {
+            _localStorage.RemoveItemAsync("authToken");
+            var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+            var authState = Task.FromResult(new AuthenticationState(anonymous));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        private ClaimsIdentity GetClaimsPrincipalFromToken(string token)
+        {
+            try
+            {
+                var jwt = _tokenHandler.ReadJwtToken(token);
+                var claims = jwt.Claims;
+
+                return new ClaimsIdentity(claims, "authToken");
+            }
+            catch
+            {
+                return new ClaimsIdentity();
+            }
+        }
+    }
+
+}
