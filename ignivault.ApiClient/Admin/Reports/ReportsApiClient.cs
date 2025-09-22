123ebb8eb0b227a064aa94dee99613eca6a17ddd
PSmith0000 <40529988+PSmith0000@users.noreply.github.com>
2025-09-22 18:33:51 -0400

    Version 0.1a
    Added new api endpoints
    Added admin tools
    Added general functionality

---
+++ ignivault.ApiClient/Admin/Reports/ReportsApiClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ignivault.Shared.DTOs.Reports;
+
+namespace ignivault.ApiClient.Admin.Reports
+{
+    public interface IReportsApiClient
+    {
+        Task<ApiResponse<FullReportDto>> GetReportsAsync();
+    }
+
+    public class ReportsApiClient : IReportsApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public ReportsApiClient(HttpClient httpClient) { _httpClient = httpClient; }
+
+        public async Task<ApiResponse<FullReportDto>> GetReportsAsync()
+        {
+            var report = await _httpClient.GetFromJsonAsync<FullReportDto>("api/reports");
+            return new ApiResponse<FullReportDto> { IsSuccess = true, Data = report };
+        }
+    }
+}
